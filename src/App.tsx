7d6134cfd921e21c2cc169fecb80b3e233c9938c
Luisinho Layme <luisinhotf@gmail.com>
2022-11-12 20:56:25 -0400

    lazyload basico hasta aqui

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Navigation from './routes/Navigation'
 
 function App() {
 
+  //TODO: 4.lazyload
+  //HACK: You are the video 39 Nested lazy Routes
+
   return (
     <>
       <Navigation />
