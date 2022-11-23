4c6701c0b80e1c66c9dce736b49e5629f3044dc3
Luisinho Layme <luisinhotf@gmail.com>
2022-11-22 21:05:54 -0400

    component extends styles

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,5 +1,6 @@
 import {} from 'react'
 import {ProductCard, ProductButtons, ProductImage, ProductTitle } from '../components'
+import '../styles/custom-styles.css'
 
 const product = {
   id: '1',
@@ -17,16 +18,36 @@ const ShoppingPage = () => {
         display: 'flex',
         flexWrap: 'wrap'
       }}>
-        <ProductCard product={ product }>
-          <ProductCard.Image />
-          <ProductCard.Title title={ 'Luisinho' } />
-          <ProductCard.Buttons />
+        <ProductCard 
+          product={ product }
+          className="bg-dark text-white"
+        >
+          <ProductCard.Image className="custom-image"/>
+          <ProductCard.Title className="text-bold" title={ 'Luisinho' } />
+          <ProductCard.Buttons className="custom-buttons"/>
         </ProductCard>
 
-        <ProductCard product={ product }>
-          <ProductImage />
-          <ProductTitle />
-          <ProductButtons />
+        <ProductCard 
+          product={ product }
+          className="bg-dark text-white"
+        >
+          <ProductImage className="custom-image"/>
+          <ProductTitle className="text-bold" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard 
+          product={ product }
+          style={{
+            backgroundColor: '#70d1f8'
+          }}
+        >
+          <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}/>
+          <ProductTitle style={{ fontWeight: 'bold' }} />
+          <ProductButtons style={{
+            display: 'flex',
+            justifyContent: 'flex-end'
+          }}/>
         </ProductCard>
       </div>
     </div>
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import {} from 'react'
 import Navigation from './routes/Navigation'
 
-//TODO: Patrones de component.
-//HACK: You are the video 53 Separando la logica en archivos independientes
 function App() {
 
   return (
