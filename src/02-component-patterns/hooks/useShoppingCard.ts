824cb71b8533cfdcb57c7c756a9a8c842940d01a
Luisinho Layme <luisinhotf@gmail.com>
2022-12-09 21:32:32 -0400

    control props alt

--- src/02-component-patterns/hooks/useShoppingCard.ts
+++ src/02-component-patterns/hooks/useShoppingCard.ts
@@ -9,31 +9,16 @@ export const useShoppingCard = () => {
     // console.log('onProductCountChange', count, product)
     setShoppingCard(oldShoppingCard => {
 
-      const productInCard: ProductInCard = oldShoppingCard[product.id] || { ...product, count: 0 }
-
-      if( Math.max( productInCard.count + count, 0 ) > 0 ) {
-        productInCard.count += count
-        return {
-          ...oldShoppingCard,
-          [product.id]: productInCard
-        }
-      }
-
-      // Borrar el producto.
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCard
-      return rest
-
       // eliminamos el product, con el count en 0
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCard
-      //   return rest
-      // }
-      //
-      // return {
-      //   ...oldShoppingCard,
-      //   [product.id]: { ...product, count }
-      // }
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCard
+        return rest
+      }
+      
+      return {
+        ...oldShoppingCard,
+        [product.id]: { ...product, count }
+      }
     })
   }
   return {
